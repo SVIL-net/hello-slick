bbe0f5f7b1378551ea90faf08b467fe15df56371
YoTuGi <yotugi1974@gmail.com>
2021-05-26 09:53:17 +0900

    Add New testbench for Exec2-function

---
+++ src/test/scala/Exercise2_test.scala
@@ -0,0 +1,56 @@
+import org.scalatest._
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Seconds, Span}
+import slick.jdbc.H2Profile.api._
+import slick.jdbc.meta._
+import scala.util.Random
+
+class Exercise2_test extends funsuite.AnyFunSuite with BeforeAndAfter with ScalaFutures {
+  implicit override val patienceConfig = PatienceConfig(timeout = Span(5, Seconds))
+
+  val suppliers = TableQuery[Suppliers]
+  val coffees = TableQuery[Coffees]
+  
+  var db: Database = _
+
+  def createSchema() =
+    db.run((suppliers.schema ++ coffees.schema).create).futureValue
+  
+  def insertSupplier() = {
+    val suppliersInsert: DBIO[Unit] = DBIO.seq(
+      suppliers += (101, "Acme, Inc.", "99 Market Street", "Groundsville", "CA", "95199"),
+      suppliers += ( 49, "Superior Coffee", "1 Party Place", "Mendocino", "CA", "95460"),
+      suppliers += (150, "The High Ground", "100 Coffee Lane", "Meadows", "CA", "93966")
+    )
+    db.run(suppliersInsert).futureValue
+  }
+
+  def insertCoffees() = {
+    val coffeesInsert: DBIO[Option[Int]] = coffees ++= Seq (
+        ("Colombian",         101, 7.99, 0, 0),
+        ("French_Roast",       49, 8.99, 0, 0),
+        ("Espresso",          150, 9.99, 0, 0),
+        ("Colombian_Decaf",   101, 8.99, 0, 0),
+        ("French_Roast_Decaf", 49, 9.99, 0, 0)
+    )
+    db.run(coffeesInsert).futureValue
+  }  
+
+  before { db = Database.forConfig("h2mem1") }
+  
+  test("Coffees table update works") {
+    createSchema()
+    insertSupplier()
+    insertCoffees()
+
+    val sale_volume = Random
+    val fix_sale_volme = sale_volume.nextInt(100)
+    val coffeenameArray = Array("Colombian", "French_Roast", "Espresso", "Colombian_Decaf", "French_Roast_Decaf")
+
+    for (value <- coffeenameArray) {
+    Excercise2.updateCoffeeTable(value,fix_sale_volme)
+    val update1 = db.run(coffees.filter(_.name === value).map(_.sales).result).futureValue
+    assert(update1.head == fix_sale_volme) 
+    }
+  }
+}
